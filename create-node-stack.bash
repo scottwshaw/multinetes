088693434aadecb77e0104e2b12cf8a2a05e9d1c
Scott Shaw <scottwshaw@gmail.com>
2019-01-08 18:31:42 +1100

    got the cluster up and running

--- create-node-stack.bash
+++ create-node-stack.bash
@@ -1,42 +1,41 @@
 #!/bin/bash
 set -e
 source vars.bash
-export PARAMETERS="[\
+export STACK_PARAMETERS="[\
             {\
-              "ParameterKey": "KeyName",\
-              "ParameterValue": "AWSKey02",\
-              "UsePreviousValue": true|false,\
-              "ResolvedValue": "string"\
+              \"ParameterKey\": \"KeyName\",\
+	      \"ParameterValue\": \"AWSKey02\"\
             },\
             {\
-              "ParameterKey": "NodeImageId",\
-              "ParameterValue": "ami-03297c04f71690a76",\
-              "UsePreviousValue": true|false,\
-              "ResolvedValue": "string"\
+              \"ParameterKey\": \"NodeImageId\",\
+              \"ParameterValue\": \"ami-03297c04f71690a76\"\
             },\
             {\
-              "ParameterKey": "NodeInstanceType",\
-              "ParameterValue": "t3.micro",\
-              "UsePreviousValue": true|false,\
-              "ResolvedValue": "string"\
+              \"ParameterKey\": \"NodeInstanceType\",\
+              \"ParameterValue\": \"t3.micro\"\
             },\
             {\
-              "ParameterKey": "ClusterName",\
-              "ParameterValue": "$CLUSTER_NAME",\
-              "UsePreviousValue": true|false,\
-              "ResolvedValue": "string"\
+              \"ParameterKey\": \"ClusterName\",\
+              \"ParameterValue\": \"$CLUSTER_NAME\"\
             },\
             {\
-              "ParameterKey": "VpcId",\       
-              "ParameterValue": "$VPC_ID",\
-              "UsePreviousValue": true|false,\
-              "ResolvedValue": "string"\
-            },\
+              \"ParameterKey\": \"VpcId\",\
+              \"ParameterValue\": \"$VPC_ID\"\
+	    },\
             {\
-              "ParameterKey": "Subnets",\       
-              "ParameterValue": "$SUBNET_IDS",\
-              "UsePreviousValue": true|false,\
-              "ResolvedValue": "string"\
+              \"ParameterKey\": \"ClusterControlPlaneSecurityGroup\",\
+              \"ParameterValue\": \"$CLUSTER_CONTROL_SECURITY_GROUP_ID\"\
+	    },\
+            {\
+              \"ParameterKey\": \"NodeGroupName\",\
+              \"ParameterValue\": \"$NODE_GROUP_NAME\"\
+	    },\
+            {\
+              \"ParameterKey\": \"Subnets\",\
+              \"ParameterValue\": \"$SUBNET_IDS\"\
             }\
           ]"
-aws cloudformation create-stack --template-url https://amazon-eks.s3-us-west-2.amazonaws.com/cloudformation/2018-12-10/amazon-eks-nodegroup.yaml 
+
+echo aws cloudformation create-stack --stack-name $EKS_NODE_STACK_NAME --template-url https://amazon-eks.s3-us-west-2.amazonaws.com/cloudformation/2018-12-10/amazon-eks-nodegroup.yaml --parameters "$STACK_PARAMETERS" --capabilities CAPABILITY_IAM
+
+aws cloudformation create-stack --stack-name $EKS_NODE_STACK_NAME --template-url https://amazon-eks.s3-us-west-2.amazonaws.com/cloudformation/2018-12-10/amazon-eks-nodegroup.yaml --parameters "$STACK_PARAMETERS" --capabilities CAPABILITY_IAM
